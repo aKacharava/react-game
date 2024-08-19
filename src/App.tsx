05e9e800d86b05f5698008a2ccc1d4ea681d780a
Alexander Kacharava <alexander@cube.nl>
2024-08-19 16:21:04 +0200

    Implement refs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import Player from "./components/Player/Player.tsx";
+
 function App() {
-  return (
-    <>
-        <h1>Create game</h1>
-    </>
-  )
+    return (
+        <div id={"content"}>
+            <header>
+                <h1>The <em>almost</em> final countdown</h1>
+                <p>Stop the timer when the time is almost expired to get the best score</p>
+            </header>
+            <Player />
+            <div id="challenges"></div>
+        </div>
+    )
 }
 
 export default App
