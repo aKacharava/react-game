1bc542d9115d26b2a03e5870ec0460ccfd267904
Alexander Kacharava <alexander@cube.nl>
2024-08-22 13:49:08 +0200

    Add winning condition for the timer challenge

--- src/components/ResultModal/ResultModal.tsx
+++ src/components/ResultModal/ResultModal.tsx
@@ -1,24 +1,38 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 
-export const ResultModal = forwardRef<{ open: () => void }, { result: string; targetTime: number }>(
-    function ResultModal(props, ref) {
-        const dialog = useRef<HTMLDialogElement>(null);
+export const ResultModal =
+    forwardRef<
+        {
+            open: () => void;
+        },
+        {
+            remainingTime: number;
+            targetTime: number;
+            onReset: () => void;
+        }
+    >(
+        function ResultModal(props, ref) {
+            const dialog = useRef<HTMLDialogElement>(null);
 
-        useImperativeHandle(ref, () => ({
-            open() {
-                dialog.current?.showModal();
-            }
-        }), []);
+            const userLost: boolean = props.remainingTime <= 0
+            const formattedRemainingTime = (props.remainingTime / 1000).toFixed(2)
+            const score = Math.round((1 - props.remainingTime / (props.targetTime * 1000)) * 100)
 
-        return (
-            <dialog ref={dialog} className="result-modal">
-                <h2>You {props.result}!</h2>
-                <p>The target time was <strong>{props.targetTime}</strong> seconds.</p>
-                <p>You stopped the timer with <strong>X seconds left.</strong></p>
-                <form method="dialog">
-                    <button>OK</button>
-                </form>
-            </dialog>
-        );
-    }
-);
+            useImperativeHandle(ref, () => ({
+                open() {
+                    dialog.current?.showModal();
+                }
+            }), []);
+
+            return (
+                <dialog ref={dialog} className="result-modal">
+                    {userLost ? <h2>You lost!</h2> : <h2>Your score: {score}</h2>}
+                    <p>The target time was <strong>{props.targetTime}</strong> seconds.</p>
+                    <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+                    <form method="dialog" onSubmit={props.onReset}>
+                        <button>OK</button>
+                    </form>
+                </dialog>
+            );
+        }
+    );
--- src/components/TimerChallenge/TimerChallenge.tsx
+++ src/components/TimerChallenge/TimerChallenge.tsx
@@ -8,41 +8,55 @@ export default function TimerChallenge(
         targetTime: number
     }
 ) {
-    const [timerExpired, setTimerExpired] = useState(false)
-    const [timerStarted, setTimerStarted] = useState(false)
+    const [timeRemaining, setTimeRemaining] = useState<number>(props.targetTime * 1000)
 
-    const timer = useRef<number | undefined>(undefined);
-    const dialog = useRef<{ open: () => void } | null>(null);
+    const timer = useRef<number | undefined>(undefined)
+    const dialog = useRef<{ open: () => void } | null>(null)
+
+    const timerIsActive: boolean = timeRemaining > 0 && timeRemaining < props.targetTime * 1000
+
+    if (timeRemaining <= 0) {
+        handleStop()
+        dialog.current?.open()
+    }
+
+    function handleReset() {
+        setTimeRemaining(props.targetTime * 1000)
+    }
 
     function handleStart() {
-        timer.current = setTimeout(() => {
-            setTimerExpired(true);
-            dialog.current?.open();
-        }, props.targetTime * 1000)
-
-        setTimerStarted(true);
+        timer.current = setInterval(() => {
+            setTimeRemaining((prevTimeRemaining: number) => {
+                return prevTimeRemaining - 10
+            });
+        }, 10)
     }
 
     function handleStop() {
-        clearTimeout(timer.current)
+        dialog.current?.open()
+        clearInterval(timer.current)
     }
 
     return(
         <>
-            <ResultModal ref={dialog} result="lost" targetTime={props.targetTime} />
+            <ResultModal
+                ref={dialog}
+                remainingTime={timeRemaining}
+                targetTime={props.targetTime}
+                onReset={handleReset}
+            />
             <section className="challenge">
                 <h2>{props.title}</h2>
-                {timerExpired && <p>Timer expired, you lost.</p>}
                 <p className="challenge-time">
                     {props.targetTime} second{props.targetTime !== 1 ? 's' : ''}
                 </p>
                 <p>
-                    <button onClick={timerStarted ? handleStop : handleStart}>
-                        {timerStarted ? 'Stop' : 'Start'} Challenge
+                    <button onClick={timerIsActive ? handleStop : handleStart}>
+                        {timerIsActive ? 'Stop' : 'Start'} Challenge
                     </button>
                 </p>
-                <p className={timerStarted ? 'active' : ''}>
-                    {timerStarted ? 'Time is running...' : 'Time inactive'}
+                <p className={timerIsActive ? 'active' : ''}>
+                    {timerIsActive ? 'Time is running...' : 'Time inactive'}
                 </p>
             </section>
         </>
